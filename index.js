b4bb395e793289f701d48919998e08de9807f25b
Cesar Mejias <cesarfar376@gmail.com>
2022-03-06 20:35:03 -0800

    muchos cambios

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// const navOptions = document.getElementById("navBarOption1");
-
+const arrow = document.querySelectorAll('.arrowM')
+const arrowM = []
+arrowM.push(...arrow)
 
 const callBurger = (event) => {
     if (event.target === burger || event.target === closeX) {
@@ -14,20 +15,23 @@ document.addEventListener('click', callBurger);
 document.addEventListener('click', (e) => {
     if (e.target === navBarOption1) {
         option1.classList.toggle('visible');
+        const arrowM1 = arrowM[0];
+        arrowM1.classList.toggle('rotate');
     }
 });
 document.addEventListener('click', (e) => {
     if (e.target === navBarOption2) {
         option2.classList.toggle('visible');
+        const arrowM2 = arrowM[1];
+        arrowM2.classList.toggle('rotate');
     }
 });
 document.addEventListener('click', (e) => {
     if (e.target === navBarOption3) {
         option3.classList.toggle('visible');
+        const arrowM3 = arrowM[2];
+        arrowM3.classList.toggle('rotate');
     }
 });
 
-// option3.addEventListener('click', (e) => {
-//     console.log(e)
-// })
 
